e2ae99fc2934e402f320a5120af485369419bf38
Michael Owolabi <michael.i.owolabi@gmail.com>
2018-10-26 10:34:51 +0100

    refactor the sales controller

--- api/src/controller/sales/index.js
+++ api/src/controller/sales/index.js
@@ -1,46 +1,46 @@
-const inMemorySalesRecord = require('../../model/sales');
+import inMemorySalesRecord from '../../model/sales';
 
-const getAllSales = (req, res)=> {
+const getAllSales = (req, res) => {
 	res.status(200).json(inMemorySalesRecord);
 };
 
 const getOneSales = (req, res) => {
-  const {id} = req.params;
-  const sales = inMemorySalesRecord.filter((theSales)=> theSales.id === parseInt(id))[0];
+	const { id } = req.params;
+	const sales = inMemorySalesRecord.filter(theSales => theSales.id === parseInt(id))[0];
 
-  if(!sales){
-    res.status(404).json('The specified sales record does not exist on this platform');
-    return;
-  }
-  
-  res.status(200).json(sales);
+	if (!sales) {
+		res.status(404).json('The specified sales record does not exist on this platform');
+		return;
+	}
+
+	res.status(200).json(sales);
 };
 
-const postSales = (req, res)=> {
-  const { productName, price, quantity } = req.body;
-  const lastId = inMemorySalesRecord.length;
-  const id = lastId + 1;
+const postSales = (req, res) => {
+	const { productName, price, quantity } = req.body;
+	const lastId = inMemorySalesRecord.length;
+	const id = lastId + 1;
 
-  const username = 'user2';
+	const username = 'user2';
 
-  const newSales = {
-    id: id,
-    productName: productName,
-    quantity: quantity,
-    price: price,
-  };
-  const amount = newSales.price * newSales.quantity;
+	const newSales = {
+		id,
+		productName,
+		quantity,
+		price,
+	};
+	const amount = newSales.price * newSales.quantity;
 
-  const newSalesRecord = {
-    id: newSales.id,
-    productName: newSales.productName,
-    quantity: newSales.quantity,
-    amount: amount,
-    attendant: username,
-  };
+	const newSalesRecord = {
+		id: newSales.id,
+		productName: newSales.productName,
+		quantity: newSales.quantity,
+		amount,
+		attendant: username,
+	};
 
-  inMemorySalesRecord.push(newSalesRecord);
-  res.status(201).json(newSalesRecord);
+	inMemorySalesRecord.push(newSalesRecord);
+	res.status(201).json(newSalesRecord);
 };
 
-module.exports = { getAllSales, getOneSales, postSales };
+export default(getAllSales, getOneSales, postSales);

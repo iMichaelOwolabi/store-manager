4e4f7d24c66138cf6280cb4a0f69a5e4aae5b64d
Michael Owolabi <michael.i.owolabi@gmail.com>
2018-11-02 13:32:05 +0100

    fix sql queries

--- api/src/utility/db.sql
+++ api/src/utility/db.sql
@@ -15,4 +15,15 @@ CREATE TABLE IF NOT EXISTS
     productImage VARCHAR(128) NOT NULL
   );
 
+CREATE TABLE IF NOT EXISTS
+  sales(
+    id SERIAL PRIMARY KEY,
+    productid INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    amount INTEGER NOT NULL,
+    userid INTEGER NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (productId) REFERENCES products (id) ON DELETE CASCADE
+  );
+
   INSERT INTO users(username,password,role) VALUES ('admin', '$2b$10$l7A2ezWuxldzri4fMiwDbeJGNNQqf5aaWlMUV4mGlmezRrHg0Y/ZG', 'superadmin');

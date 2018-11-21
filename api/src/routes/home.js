428c418389fd0144397894cd429dab5c6ab820f4
Michael Owolabi <michael.i.owolabi@gmail.com>
2018-11-21 21:59:08 +0100

    update response of the default endpoint of the api

--- api/src/routes/home.js
+++ api/src/routes/home.js
@@ -3,7 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/api/v1', (req, res) => {
-  res.send('Hello there, this is the store manager API');
+  res.status(200).send({
+    success: true,
+    message: 'Hello, welcome to the store manager',
+  });
 });
 
 export default(router);

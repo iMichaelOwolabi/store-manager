473aeda437f13660e64157f2a661aeaf696f8e5a
Michael Owolabi <michael.i.owolabi@gmail.com>
2018-11-23 01:19:32 +0100

    add cors to allow cross origin resource sharing

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-
 import bodyParser from 'body-parser';
-
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
 
